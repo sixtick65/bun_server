2193cc1949c08dba63b69a0392a231ba3606d34e
sixtick_pico <sixtick65@gmail.com>
2026-05-28 18:10:25 +0900

    serve 삽입

--- index.ts
+++ index.ts
@@ -1,1 +1,40 @@
-console.log("Hello via Bun!!!");
+const server = Bun.serve({
+  port: 3443,
+  routes: {
+    // Static route for the root path
+    "/": new Response("Hello, World! Welcome to Bun!"),
+
+    // Static routes
+    "/api/status": new Response("OK"),
+
+    // Dynamic routes
+    "/users/:id": req => {
+      return new Response(`Hello User ${req.params.id}!`);
+    },
+
+    // Per-HTTP method handlers
+    "/api/posts": {
+      GET: () => new Response("List posts"),
+      POST: async req => {
+        const body = await req.json();
+        return Response.json({ created: true, ...body });
+      },
+    },
+
+    // Wildcard route for all routes that start with "/api/" and aren't otherwise matched
+    "/api/*": Response.json({ message: "Not found" }, { status: 404 }),
+
+    // Redirect from /blog/hello to /blog/hello/world
+    "/blog/hello": Response.redirect("/blog/hello/world"),
+
+    // Serve a file by lazily loading it into memory
+    "/favicon.ico": Bun.file("./favicon.ico"),
+  },
+
+  // 라우트에 걸리지 않으면 404 Not Found 응답을 반환하는 기본 핸들러
+  fetch(req) {
+    return new Response("Not Found", { status: 404 });
+  },
+});
+
+console.log(`Server running at ${server.url}`);
